df7226871eccfdf85ba81894b9491bf1e4973ed7
Peyton Critchfield <peyton.developer@gmail.com>
2021-09-06 13:55:21 -0400

    board setup

--- src/App.vue
+++ src/App.vue
@@ -8,9 +8,12 @@
       v-bind:key="index"
       v-bind:class="{spaces2: !boardSpaces[index].color}"
       v-on:click="moveStart(index)">
-      
+
+        <img :src="imageSrc[index]" width="40" height="40"/>
 
       </div>
+
+        
     </div>
 
   </div>
@@ -23,7 +26,21 @@ export default {
   components: {},
   data() {
     return {
-      boardSpaces: []
+      boardSpaces: [],
+      imageSrc: [],
+      darkPieceSetUp: ['death-knight', 'dire-wolf', 'dragon',
+                       'ogre', 'lich', 'dark-castle', 'ogre',
+                       'dragon', 'dire-wolf', 'death-knight',
+                       'orc', 'skeleton', 'orc', 'skeleton',
+                       'orc', 'skeleton', 'orc', 'skeleton',
+                       'orc', 'skeleton'],
+      lightPieceSetUp: ['soldier', 'elf', 'soldier',
+                       'elf', 'soldier', 'elf', 'soldier',
+                       'elf', 'soldier', 'elf',
+                        'knight', 'lion', 'griffin',
+                       'giant', 'light-castle', 'paladin', 'giant',
+                       'griffin', 'lion', 'knight'],
+      moveActivated: false
     }
   },
   methods: {
@@ -33,25 +50,44 @@ export default {
           let spaces = {
             x: `${i + 1}`,
             y: `${j + 1}`,
-            id: `(${i + 1},${j + 1})`
+            index: i + 10 * j
           }
           if ((i - j) % 2 === 0 || (j - i) % 2 === 0)
           {
             spaces.color = true
-          } 
-          
-
+          } else {
+            spaces.color = false
+          }
           this.boardSpaces.push(spaces)
         }
       }
     },
+    createPieces() {
+      for(let i = 0; i < 100; i++) 
+      {
+        this.imageSrc.push(require('./assets/blank.png'))
+      }
+      for(let i = 0; i < 20; i++) {
+        this.imageSrc.splice(i, 1, require(`./assets/dark/${this.darkPieceSetUp[i]}.png`))
+      }
+      for(let i = 0; i < 20; i++) {
+        this.imageSrc.splice(i + 80, 1, require(`./assets/light/${this.lightPieceSetUp[i]}.png`))
+      }
+    },
     moveStart(index) {
-      index.innerHTML = 'hello'
+      this.moveActivated = !this.moveActivated
+      const spaceSelected = this.boardSpaces.find((child) => child.index === index)
+      console.log(spaceSelected)
+      if(this.moveActivated === true){
+        console.log('ready to move')
+      }
     }
   },
+
   created() {
     this.createBoard()
-
+    this.createPieces()
+    console.log(this.boardSpaces)
   }
 }
 </script>
@@ -59,7 +95,7 @@ export default {
 <style>
 body {
   margin: 0;
-  background-color: #2c3e50;
+  background-color: rgb(252, 252, 252);
 }
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
@@ -67,25 +103,32 @@ body {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 }
 .spaces-container {
+  margin-top: 10px;
   display: flex;
   flex-wrap: wrap;
   justify-content: center;
   align-items: center;
+  min-width: 500px;
+  max-width: 650px
 }
 .spaces1 {
-  background-color: #2c3e50;
-  border: 1px solid white;
+  background-color: #02779b;
   display: flex;
-  width: 9vw;
-  height: 9vw;
+  width: 10vw;
+  height: 10vw;
   min-width: 50px;
   min-height: 50px;
+  max-width: 65px;
+  max-height: 65px;
   justify-content: center;
   align-items: center;
 }
 .spaces2 {
-  background-color: black;
+  background-color: rgba(128, 127, 127, 0.5);
 }
 </style>
